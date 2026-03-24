e2a2a81b894833b5f3aebf1075ef4c841d6470fb
Amiya351 <amiyadas351@gmail.com>
2026-03-25 00:21:24 +0530

    added the docker compose file

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # app/main.py
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from dotenv import load_dotenv
 
 # load environment variables from .env file
@@ -9,11 +9,15 @@ load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "default_key")
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        username = request.form.get("username")
+        return render_template("result.html", username=username)
     return render_template("index.html")
 
-if __name__ == '__main__':
-    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
+#if __name__ == '__main__':
+#    debug_mode = os.getenv("FLASK_DEBUG", "False") == "True"
     port = int(os.getenv("FLASK_PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+#    app.run(host='0.0.0.0', port=port, debug=debug_mode)
+
